0affba7259ba893b0b6c6cc540101fc9e33e1bac
Ashish Sapkota <asissapkota42@gmail.com>
2023-08-11 19:19:00 +0545

    all family count

--- lib/screens/allfamily.dart
+++ lib/screens/allfamily.dart
@@ -51,93 +51,109 @@ class _AllFamilyPageState extends State<AllFamilyPage> {
         child: Text('No families found'),
       );
     } else {
-      return Padding(
-        padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 60.0),
-        child: ListView.builder(
-          itemCount: familyData.length,
-          itemBuilder: (context, index) {
-        final family = familyData[index];
-        final name = family['name'];
-        final hiCode = family['hiCode'];
-        final phoneNo = family['phnNo'];
-        final amount = family['annualFee'];
-        return Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Colors.blueAccent),
-              borderRadius: BorderRadius.circular(8)),
-          child: SafeArea(
-            bottom: true,
-            child: ListTile(
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FamilyDetailsPage(hiCode: hiCode.toString())),
-                );
-              },
-              contentPadding:const EdgeInsets.all(8),
-              title: Text('$name', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('$hiCode', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Contact: $phoneNo'),
-                            Text('Amount: $amount')
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Row(
-                              children: [
-                                ElevatedButton(
-                                    style: ButtonStyle(
-                                        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF1a457c))
-                                    ),
-                                    onPressed: (){
-                                      final Uri uri = Uri(
-                                        scheme: 'tel',
-                                        path: '+977$phoneNo',
-                                      );
-                                      _launchUrl(uri);
-                                    },
-                                    child: const Text('Call')
-                                ),
-                                const SizedBox(width: 8,),
-                                ElevatedButton(
-                                    style: ButtonStyle(
-                                        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF1a457c))
-                                    ),
-                                    onPressed: (){
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => FamilyDetailsPage(hiCode: hiCode.toString())),
-                                        );
-                                    },
-                                    child: const Text('Details')
-                                ),
-                              ],
-                            )
-                          ],
-                        )
-                      ],
-                    )
-                    ),
-                ],
-              ),
-              // Add more fields as needed
+      int familyLength = familyData.length;
+      return SingleChildScrollView(
+        child:
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('$familyLength families'),
             ),
-          ),
-        );
-          },
-        ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, bottom: 60.0),
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: familyData.length,
+                itemBuilder: (context, index) {
+              final family = familyData[index];
+              final name = family['name'];
+              final hiCode = family['hiCode'];
+              final phoneNo = family['phnNo'];
+              final amount = family['annualFee'];
+              return Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.blueAccent),
+                    borderRadius: BorderRadius.circular(8)),
+                child: SafeArea(
+                  bottom: true,
+                  child: ListTile(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FamilyDetailsPage(hiCode: hiCode.toString())),
+                      );
+                    },
+                    contentPadding:const EdgeInsets.all(8),
+                    title: Text('$name', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('$hiCode', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Contact: $phoneNo'),
+                                  Text('Amount: $amount')
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      ElevatedButton(
+                                          style: ButtonStyle(
+                                              backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF1a457c))
+                                          ),
+                                          onPressed: (){
+                                            final Uri uri = Uri(
+                                              scheme: 'tel',
+                                              path: '+977$phoneNo',
+                                            );
+                                            _launchUrl(uri);
+                                          },
+                                          child: const Text('Call')
+                                      ),
+                                      const SizedBox(width: 8,),
+                                      ElevatedButton(
+                                          style: ButtonStyle(
+                                              backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF1a457c))
+                                          ),
+                                          onPressed: (){
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(builder: (context) => FamilyDetailsPage(hiCode: hiCode.toString())),
+                                              );
+                                          },
+                                          child: const Text('Details')
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                          ),
+                      ],
+                    ),
+                    // Add more fields as needed
+                  ),
+                ),
+              );
+                },
+              ),
+            ),
+          ],
+        )
       );
     }
   }
